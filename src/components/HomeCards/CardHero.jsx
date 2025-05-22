683899496caeb725a354c689cfe49230f563007b
sagor549 <sagorghosh9899@gmail.com>
2025-05-22 20:48:52 +0530

    mkdemfkd

--- src/components/HomeCards/CardHero.jsx
+++ src/components/HomeCards/CardHero.jsx
@@ -45,7 +45,7 @@ export default function CardHero() {
                   {
                     icon: 'M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z',
                     title: 'AgriSense App',
-                    desc: 'Advanced precision agriculture for  growing'
+                    desc: 'Advanced precision agriculture for crop  growing'
                   },
                   {
                     icon: 'M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z',
